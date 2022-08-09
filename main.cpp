9527b5458470e709cfb70a90ba0de961cb08b5be
OlegM <mov20@mail.ru>
2022-08-09 16:28:10 +0300

    Added hello world cpp file and cmake

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    // magic constant to print in Hello World
+    // next line of strange comments
+
+    const int i = 28*45; 
+    std::cout << "Hello World! " << i << std::endl;
+    return 0;
+}
+
